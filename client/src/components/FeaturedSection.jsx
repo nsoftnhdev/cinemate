8254de5a978876cd22bc814efb540e4e6781ef85
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2025-07-30 18:02:22 +0800

    function added to release seats after 10 min

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -23,7 +23,7 @@ const FeaturedSection = () => {
       </div>
 
       <div className="flex flex-wrap max-sm:justify-center gap-8 mt-8">
-        {shows.slice(0, 5).map((show) => (
+        {shows.slice(0, 4).map((show) => (
           <MovieCard key={show._id} movie={show} />
         ))}
       </div>
--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -3,31 +3,37 @@ import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 
 const HeroSection = () => {
-
   const navigate = useNavigate();
 
   return (
-    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.png")] bg-cover bg-center h-screen'>
+    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.png")] bg-cover h-screen bg-top'>
       <img src={assets.marvelLogo} alt="" className="max-h-11 lg:h-11 mt-20" />
 
       <h1 className="text-5xl md:text-[70px] md:leading-18 font-semibold maw-w-110">
-        Fantastic <br /> 4
+        IRONHEART
+        <br />
       </h1>
 
       <div className="flex items-center gap-4 text-gray-300">
         <span>Action | Adventure | Sci-Fi</span>
         <div className="flex items-center gap-1">
-          <CalendarIcon className="w-4.5 h-4.5" /> 2018
+          <CalendarIcon className="w-4.5 h-4.5" /> 2025
         </div>
         <div className="flex items-center gap-1">
-          <ClockIcon className="w-4.5 h-4.5" /> 2h 8m
+          <ClockIcon className="w-4.5 h-4.5" /> 1 Seasons - 6 Episodes
         </div>
       </div>
       <p className="max-w-md text-gray-300">
-        In a post-apocalyptic world where cities ride on wheels and consume each
-        other to survive, two people meet in London and try to stop conspiracy.
+        Riri Williams, a young, genius inventor determined to
+        make her mark on the world, returns to her hometown of Chicago. Her
+        unique take on building iron suits is brilliant, but in pursuit of her
+        ambitions, she finds herself wrapped up with the mysterious yet charming
+        Parker Robbins aka "The Hood."
       </p>
-      <button onClick={() => navigate("/movies")} className="flex items-center gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">
+      <button
+        onClick={() => navigate("/movies")}
+        className="flex items-center gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+      >
         Explore Movies
         <ArrowRight className="w-5 h-5" />
       </button>
