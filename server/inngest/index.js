4d7558737cff881434866e5d6d10ab253d592060
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2025-07-31 01:20:29 +0800

    booking link updated

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -223,7 +223,7 @@ const sendShowReminders = inngest.createFunction(
           </p>
 
           <div style="text-align: center;">
-            <a href="https://cinemate-peach.vercel.app/my-bookings" style="background-color: #FFA726; color: #fff; padding: 12px 25px; text-decoration: none; border-radius: 5px; font-weight: bold;">
+            <a href="https://cinemate-q6ije1ejx-nsoftnhdevs-projects.vercel.app/my-bookings" style="background-color: #FFA726; color: #fff; padding: 12px 25px; text-decoration: none; border-radius: 5px; font-weight: bold;">
               View Booking
             </a>
           </div>
