67a1a2c220805eadc3404af962ed9bbfce087e81
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2025-07-31 11:44:14 +0800

    background image file format changed

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
   const navigate = useNavigate();
 
   return (
-    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.png")] bg-cover h-screen bg-top'>
+    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.webp")] bg-cover h-screen bg-top'>
       <img src={assets.marvelLogo} alt="" className="max-h-11 lg:h-11 mt-20" />
 
       <h1 className="text-5xl md:text-[70px] md:leading-18 font-semibold maw-w-110">
